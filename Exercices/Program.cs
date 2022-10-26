a0914717517c44565571b8acafbadf85ca391339
Damien Zeewolfed <falkodamiendev@gmail.com>
2022-10-26 12:44:04 +0100

    Revert "lololol"
    This reverts commit 4d64cd58c0aec893f7b79444f6b396592113534b.

--- Exercices/Program.cs
+++ Exercices/Program.cs
@@ -49,7 +49,7 @@ namespace exercices
             }
             else
             {
-                Console.WriteLine("You wan a Kitten!lolololol");
+                Console.WriteLine("You wan a Kitten!hiphiphip");
             }
 
             return result;
