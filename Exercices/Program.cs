4d64cd58c0aec893f7b79444f6b396592113534b
Damien Zeewolfed <falkodamiendev@gmail.com>
2022-10-26 12:29:19 +0100

    lololol

--- Exercices/Program.cs
+++ Exercices/Program.cs
@@ -49,7 +49,7 @@ namespace exercices
             }
             else
             {
-                Console.WriteLine("You wan a Kitten!hiphiphip");
+                Console.WriteLine("You wan a Kitten!lolololol");
             }
 
             return result;
